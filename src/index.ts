6d94677828d584d1bacf25a9c750990fe7ea7e6e
Kingsley Victor <javaprodigy56@gmail.com>
2023-08-02 01:06:08 +0100

    ..

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import "./styles/index.css";
 import Button from "./components/Button";
+import DispatchView from "./views/dispatch";
 
-export { Button as KwikPikCustomButton };
+export { Button as KwikPikCustomButton, DispatchView as KwikPikDispatchView };
--- src/views/Base/index.tsx
+++ src/views/Base/index.tsx
@@ -7,10 +7,10 @@ interface ViewBaseCardProps {
 
 export default function ViewBaseCard({ children }: ViewBaseCardProps) {
   return (
-    <div className="w-screen min-h-screen fixed flex justify-center items-center backdrop-brightness-[.3] z-20 px-4 py-4 overflow-auto">
-      <div className="shadow-[0px_4px_20px_0px_rgba(0,_0,_0,_0.05)] bg-[#fff] rounded-[0.5rem] flex flex-col w-full md:w-1/3 justify-start items-center gap-4 py-7 px-7 md:px-12 overflow-auto">
-        <img src={logo} className="w-[200px] h-24" alt="kwikpik_logo" />
-        <div className="w-full">{children}</div>
+    <div className="w-screen min-h-screen absolute flex justify-center items-center backdrop-brightness-[.3] z-20 px-2 py-4 overflow-auto">
+      <div className="shadow-[0px_4px_20px_0px_rgba(0,_0,_0,_0.05)] bg-[#fff] rounded-[0.5rem] flex flex-col w-full md:w-1/3 justify-start items-center gap-4 py-7 px-7 md:px-12">
+        <img src={logo} className="w-[150px] h-24" alt="kwikpik_logo" />
+        <div className="w-full overflow-auto flex-1">{children}</div>
       </div>
     </div>
   );
