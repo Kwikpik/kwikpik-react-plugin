6bde29c94836375a95b1c3be7c30f65e4f9d85b9
Kingsley Victor <javaprodigy56@gmail.com>
2023-09-09 15:54:37 +0100

    ...

--- src/views/Base/index.tsx
+++ src/views/Base/index.tsx
@@ -9,8 +9,8 @@ interface ViewBaseCardProps {
 
 export default function ViewBaseCard({ children, onClose }: ViewBaseCardProps) {
   return (
-    <div className="w-screen min-h-screen absolute flex flex-col-reverse md:flex-row justify-start md:justify-center items-center md:items-start gap-5 backdrop-brightness-[.3] z-20 px-3 py-8 overflow-auto">
-      <div className="shadow-[0px_4px_20px_0px_rgba(0,_0,_0,_0.05)] bg-[#fff] rounded-[0.5rem] flex flex-col w-full md:w-1/3 justify-start items-center gap-4 py-7 px-7 md:px-12 overflow-auto">
+    <div className="w-screen min-h-screen fixed flex flex-col-reverse md:flex-row justify-start md:justify-center items-center md:items-start gap-5 backdrop-brightness-[.5] z-20 px-3 py-8 overflow-auto">
+      <div className="shadow-[0px_4px_20px_0px_rgba(0,_0,_0,_0.05)] bg-[#fff] rounded-[0.5rem] flex flex-col w-full md:w-1/3 justify-start items-center gap-4 py-7 px-7 md:px-12 overflow-auto z-20">
         <img src={logo} className="w-[150px] h-24" alt="kwikpik_logo" />
         <div className="w-full overflow-auto">{children}</div>
       </div>
